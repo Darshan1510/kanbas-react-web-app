b71597f11fde310b289fc47cbfd2e7a25d7fcbf7
Darshan shah <darshan@mytradezone.com>
2024-02-08 20:19:16 -0500

    Kanbas pages with CSS/Bootstrap

--- public/Kanbas/Navigation/index.js
+++ public/Kanbas/Navigation/index.js
@@ -3,48 +3,67 @@ function KanbasNavigation(location) {
     {
       name: "Account",
       url: "/Kanbas/Account/Profile/screen.html",
+      icon: "fa fa-user-circle",
     },
     {
       name: "Dashboard",
       url: "/Kanbas/Dashboard/screen.html",
+      icon: "fa fa-tachometer",
     },
     {
       name: "Courses",
       url: "/Kanbas/Courses/Home/screen.html",
+      icon: "fa fa-fax",
     },
     {
       name: "Calendar",
       url: "#",
+      icon: "fa fa-calendar",
     },
     {
       name: "Inbox",
       url: "#",
+      icon: "fa fa-inbox",
     },
     {
       name: "History",
       url: "#",
+      icon: "fa fa-history",
     },
     {
       name: "Studio",
       url: "#",
+      icon: "fa fa-toggle-right",
     },
     {
       name: "Commons",
       url: "#",
+      icon: "fa fa-share-square-o",
     },
     {
       name: "Help",
       url: "#",
+      icon: "fa fa-question-circle",
     },
   ];
   return `
-          <ul>
+          <ul class="wd-kanbas-navigation">
+            <li>
+              <a href="http://northeastern.edu">
+                  <img src="https://imgur.com/TFsDSJy.jpg" alt="Northeastern University Logo">
+              </a>
+            </li>
           ${links
             .map((link) => {
               return `
-              <li><a style="color:${link.name === location ? "red" : "black"}" href="${link.url}">${
-                link.name
-              }</a></li>
+              <li class="${link.name === location ? "wd-active" : ""}">
+              <a href="${link.url}"> ${
+                link.icon
+                  ? `<i class="${link.icon}" style=${
+                      link.name === "Account" ? "color:white" : ""
+                    }></i><br/>`
+                  : ""
+              } ${link.name}</a></li>
                   `;
             })
             .join("")}
